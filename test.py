d860506b90f5f1f3fa4056fb12156ea26632f0ab
Stefan Kleini <sKleini@users.noreply.github.com>
2024-09-28 21:50:25 +0200

    update test.py

--- test.py
+++ test.py
@@ -1,18 +1,44 @@
-def potenziere_mit_zwei(zahl):
-  """Potenziert eine Zahl mit 2.
+def ist_primzahl(zahl):
+  """Überprüft, ob eine Zahl eine Primzahl ist.
 
   Args:
-    zahl: Die Zahl, die potenziert werden soll.
+    zahl: Die zu überprüfende Zahl.
 
   Returns:
-    Das Ergebnis der Potenzierung.
+    True, wenn die Zahl eine Primzahl ist, andernfalls False.
   """
 
-  return zahl ** 2
+  if zahl <= 1:
+    return False
+  if zahl <= 3:
+    return True
+  if zahl % 2 == 0 or zahl % 3 == 0:
+    return False
+  i = 5
+  while i * i <= zahl:
+    if zahl % i == 0 or zahl % (i + 2) == 0:
+      return False
+    i += 6
+  return True
 
-# Benutzer nach einer Zahl fragen
-eingabe = float(input("Gib eine Zahl ein: "))
+def finde_hoechste_primzahl(obergrenze):
+  """Findet die höchste Primzahl bis zu einer gegebenen Obergrenze.
 
-# Zahl mit 2 potenzieren und Ergebnis ausgeben
-ergebnis = potenziere_mit_zwei(eingabe)
-print("Das Quadrat von", eingabe, "ist", ergebnis)
+  Args:
+    obergrenze: Die obere Grenze der Suche.
+
+  Returns:
+    Die höchste gefundene Primzahl.
+  """
+
+  hoechste_primzahl = 0
+  for zahl in range(obergrenze, 1, -1):
+    if ist_primzahl(zahl):
+      hoechste_primzahl = zahl
+      break
+  return hoechste_primzahl
+
+# Hauptprogramm
+obergrenze = 1000000
+hoechste_primzahl = finde_hoechste_primzahl(obergrenze)
+print("Die höchste Primzahl unter", obergrenze, "ist:", hoechste_primzahl)
